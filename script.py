9e4da3e2ba211dceeb5f0116d8ff43cfb5465786
Carlos Martinez <c.martinez@esciencecenter.nl>
2020-05-27 10:36:35 +0200

    Added new functionality

--- script.py
+++ script.py
@@ -3,6 +3,15 @@ import argparse
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--n', action='append', type=int)
 
+parser.add_argument('--sum', action='store_true')
+parser.add_argument('--mult', action='store_true')
+
 args = parser.parse_args()
 
-print(sum(args.n))
+if(args.sum):
+    print(sum(args.n))
+elif (args.mult):
+    acc = 1
+    for n in args.n:
+        acc *= n
+    print(acc)
